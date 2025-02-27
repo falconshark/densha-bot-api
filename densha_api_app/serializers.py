1eb99a450dac4e84ca02015d82f48b95c07b73e2
Sardo <sardo.ip@sardo.work>
2025-02-27 16:06:00 +0900

    Remove __pycache__, able to loading route stauts

--- densha_api_app/serializers.py
+++ densha_api_app/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+from .route_status import load_status
 from .models import Route 
-
 class RouteSerializer(serializers.ModelSerializer):
+    route_status = serializers.SerializerMethodField()
     class Meta:
         model = Route 
-        fields = '__all__'
+        fields = ('id', 'route_name', 'route_url', 'route_status')
+    def get_route_status(self, obj):
+       return load_status(obj.route_url)    
--- densha_api_app/views.py
+++ densha_api_app/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from .models import Route
 from .serializers import RouteSerializer
+
     
 class RouteView(viewsets.ModelViewSet):
     filter_backends = [SearchFilter]
